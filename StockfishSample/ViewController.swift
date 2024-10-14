6d1846adcbfc0eb5d526c72af6285d5700499585
Hosung.Kim <hosungkim5108@gmail.com>
2024-10-14 19:59:02 +0900

    initial

---
+++ StockfishSample/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  StockfishSample
+//
+//  Created by 김호성 on 2024.10.13.
+//
+
+import UIKit
+import Combine
+
+class ViewController: UIViewController {
+
+    private var chessEngine = ChessEngine()
+    private let sendFen = "r2q1rk1/1p2ppbp/3p2p1/p1pPn1B1/P1B1Q3/3P3P/1PP2PP1/R4RK1 b - - 0 13"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    @IBAction func onClick(_ sender: Any) {
+        
+        Task {
+            await chessEngine.sendCommand("position fen \(sendFen);eval")
+        }
+        
+    }
+    private var cancellables = Set<AnyCancellable>()
+}
+
+class ChessEngine {
+    private let stockfishWrapper = StockfishWrapper()
+        
+        
+    init() {
+        stockfishWrapper.startEngine()
+        stockfishWrapper.onResponse = { [weak self] output in
+            guard let self = self else { return }
+            if let output = output {
+                NSLog(output)
+                
+            }
+        }
+        sendInitCommand()
+    }
+
+    private func sendInitCommand() {
+        Task {
+            await sendCommand("uci")
+            await sendCommand("isready")
+        }
+    }
+
+    func sendCommand(_ command: String) async {
+        stockfishWrapper.sendCommand(command)
+    }
+
+}
